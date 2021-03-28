1980d6fc1bd24db8b658fbf9cf4cfdd3b0057cbb
Nguyen Van Huyen <phillipng.working@gmail.com>
2021-03-28 13:56:24 +0700

    folder and add style assets

--- hanuairline_react/src/admin/AdminApp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const AdminApp = () => {
-    return (
-        <div>
-            Admin space works...
-        </div>
-    )
-}
---
+++ hanuairline_react/src/api/index.js
@@ -0,0 +1,7 @@
+//TODO: list all api endpoint here
+const API = Object.freeze({
+    login: '/login',
+    logout: '/logout'
+});
+
+export default API;
